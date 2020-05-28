2f3a6539a4b1c400f62a06db7605f77fab6eb0c5
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-05-29 08:18:30 +1000

    wip on a new Handler

--- gherkin_cum/gherkin_cum/da_func_test.py
+++ gherkin_cum/gherkin_cum/da_func_test.py
@@ -1,4 +1,5 @@
-
+import sys
+import os
 
 # to parse a da's feature file:
 # read file into a buf
@@ -6,3 +7,12 @@
 # jsonify the resulting dict
 
 # i have to write a visitor to make use of the resulting dict/tree
+
+class Handler:
+
+    def handle(self, node):
+        pass
+
+
+if __name__ == '__main__':
+    this_file = ''
