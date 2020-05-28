fe8e89cf3a4fee6ae228f67a7038745640851013
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-05-29 09:21:13 +1000

    handler

--- gherkin_cum/gherkin_cum/da_func_test.py
+++ gherkin_cum/gherkin_cum/da_func_test.py
@@ -1,18 +1,70 @@
 import sys
 import os
 
-# to parse a da's feature file:
-# read file into a buf
-# pass the buf to the parser
-# jsonify the resulting dict
+from gherkin.token_scanner import TokenScanner
+from gherkin.parser import Parser
 
-# i have to write a visitor to make use of the resulting dict/tree
 
 class Handler:
 
+    @staticmethod
+    def read_and_handle(filename):
+        parser = Parser()
+        h = Handler()
+        with open(filename, 'r') as fp:
+            return h.handle(parser.parse(fp.read()))
+
     def handle(self, node):
+        meth = getattr(self, '_handle_{}'.format(node['type']), None)
+        assert meth, node['type']
+        meth(node)  # pylint: disable=not-callable
+
+    def _handle_GherkinDocument(self, node):
+        feature = node['feature']
+        for c in node.get('comments', []):
+            self.handle(c)
+        if feature:
+            self.handle(feature)
+
+    def _handle_Comment(self, node):
+        # 'type', 'location', 'text'
         pass
 
+    def _handle_Feature(self, node):
+        for child in node.get('children', []):
+            self.handle(child)
+
+    def _handle_Background(self, node):
+        __ = node['steps']
+
+    def _handle_ScenarioOutline(self, node):
+        _ = node['tags']
+        __ = node['steps']
+        for example in node.get('examples', []):
+            self.handle(example)
+
+    def _handle_Scenario(self, node):
+        _ = node['steps']
+        __ = node['steps']
+
+    def _handle_Examples(self, node):
+        _ = node['tags']
+        __ = node['tableHeader']
+        for r in node.get('tableBody', []):
+            self.handle(r)
+
+    def _handle_TableRow(self, node):
+        for c in node.get('cells', []):
+            self.handle(c)
+
+    def _handle_TableCell(self, node):
+        _ = node['value']
+
+def prt_members(node):
+    print('{}: {}'.format(node['type'], node.keys()))
 
 if __name__ == '__main__':
-    this_file = ''
+    this_file = os.path.abspath(sys.argv[0])
+    this_dir = os.path.dirname(this_file)
+    fea_file = os.path.join(this_dir, '..', 'resources', 'CEB_Dependencies.feature')
+    some = Handler.read_and_handle(fea_file)
